1f05f5205b5bbad040357243a6cb0b5c7b90dca4
Chris <Chris@CHRIS_LAPTOP>
2016-04-19 19:03:56 -0400

    Another commit for git testing and mvc learning

--- TreeTrackerNet/TreeTrackerNet/Controllers/HelloWorldController.cs
+++ TreeTrackerNet/TreeTrackerNet/Controllers/HelloWorldController.cs
@@ -9,15 +9,17 @@ namespace TreeTrackerNet.Controllers
     public class HelloWorldController : Controller
     {
         // GET: HelloWorld
-        public string Index()
+        public ActionResult Index()
         {
-            return "This is my <b>default </b> action";
+            return View();
         }
 
         // GET HelloWorld/Welcome
-        public string Welcome(string name, int num_times=1)
+        public ActionResult Welcome(string name, int num_times=1)
         {
-            return HttpUtility.HtmlEncode("Hello " + name + " Num times = " + num_times);
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = num_times;
+            return View();
         }
         // GET HelloWorld/IdCheck
         public string IdCheck(string name, int ID = 1)
