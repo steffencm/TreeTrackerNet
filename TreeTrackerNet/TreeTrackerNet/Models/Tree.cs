03829b8a1b49dc135799111bbf7207a506887256
Chris <Chris@CHRIS_LAPTOP>
2016-04-19 19:16:52 -0400

    Finish Model Section

---
+++ TreeTrackerNet/TreeTrackerNet/Models/Tree.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace TreeTrackerNet.Models
+{
+    public class Tree
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Species { get; set; }
+        public DateTime DateBought { get; set; } 
+    }
+
+    public class TreeDBContext : DbContext
+    {
+        public DbSet<Tree> Trees { get; set; }
+    }
+}
