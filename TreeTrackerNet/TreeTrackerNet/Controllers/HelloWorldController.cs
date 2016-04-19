e2118a683c7c3c95f84d29e6b8e2cd7fa641eba1
Chris <Chris@CHRIS_LAPTOP>
2016-04-18 20:56:21 -0400

    Intial commit, will probalbly delete like all of this

---
+++ TreeTrackerNet/TreeTrackerNet/Controllers/HelloWorldController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TreeTrackerNet.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public string Index()
+        {
+            return "This is my <b>default </b> action";
+        }
+
+        // GET HelloWorld/Welcome
+        public string Welcome(string name, int num_times=1)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + " Num times = " + num_times);
+        }
+        // GET HelloWorld/IdCheck
+        public string IdCheck(string name, int ID = 1)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + " Id equals " + ID); 
+        }
+    }
+}
