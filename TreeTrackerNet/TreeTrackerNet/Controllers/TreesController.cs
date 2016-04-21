ce47d6c1a1e97627d28b1990b54b58ce4de1d1af
Chris <Chris@CHRIS_LAPTOP>
2016-04-20 20:15:50 -0400

    Add controller and views for Tree

---
+++ TreeTrackerNet/TreeTrackerNet/Controllers/TreesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TreeTrackerNet.Models;
+
+namespace TreeTrackerNet.Controllers
+{
+    public class TreesController : Controller
+    {
+        private TreeDBContext db = new TreeDBContext();
+
+        // GET: Trees
+        public ActionResult Index()
+        {
+            return View(db.Trees.ToList());
+        }
+
+        // GET: Trees/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tree tree = db.Trees.Find(id);
+            if (tree == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tree);
+        }
+
+        // GET: Trees/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Trees/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,Species,DateBought")] Tree tree)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Trees.Add(tree);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tree);
+        }
+
+        // GET: Trees/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tree tree = db.Trees.Find(id);
+            if (tree == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tree);
+        }
+
+        // POST: Trees/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,Species,DateBought")] Tree tree)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tree).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tree);
+        }
+
+        // GET: Trees/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tree tree = db.Trees.Find(id);
+            if (tree == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tree);
+        }
+
+        // POST: Trees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Tree tree = db.Trees.Find(id);
+            db.Trees.Remove(tree);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
